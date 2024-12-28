001fe81147300e4db5411115649dc85fe1c22714
Imran Afzal Pansota <111197710+imranAfzal43A@users.noreply.github.com>
2024-12-28 11:57:12 +0500

    updated

--- components/TafsirTexts.tsx
+++ components/TafsirTexts.tsx
@@ -1,14 +1,18 @@
 import { Text } from "react-native";
 import { FlashList } from "@shopify/flash-list";
+import { useCallback } from "react";
 const TafsirTexts = ({ tafsir }: { tafsir: string[] }) => {
+  const renderItem=useCallback(({item})=>{
+    return(
+      <Text className="text-sm text-white mb-2 font-AnekBangla">{item}</Text>
+    )
+  },[])
   return (
     <FlashList
       data={tafsir}
-      estimatedItemSize={120}
+      estimatedItemSize={150}
       keyExtractor={(item, index) => index.toString()}
-      renderItem={({ item }) => (
-        <Text className="text-sm text-white mb-2 font-AnekBangla">{item}</Text>
-      )}
+      renderItem={renderItem}
     />
   );
 };
